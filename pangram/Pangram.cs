fe8fe634347ba6734aa40d52648c936901727f42
cprobbie23 <cprobbie23@gmail.com>
2018-07-30 23:49:56 +1000

    Pangram exercise

---
+++ pangram/Pangram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Pangram
+{
+    static string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    public static bool IsPangram(string input)
+    {
+        char[] alphabetArray = alphabet.ToCharArray();
+        int counter = 0;
+        bool result;
+        foreach (char s in alphabetArray)
+        {
+            counter = input.ToLower().Contains(s.ToString()) ? counter + 1 : counter;
+        }
+        return result = (counter == alphabet.Length) ? true : false;
+    }
+}
